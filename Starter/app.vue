2837dfcd3f83786fc38b5583f651fa1b5086b1ef
arcii-py <63791186+arcii-py@users.noreply.github.com>
2023-08-20 15:41:26 +0200

    supabase without pwa test

--- Starter/app.vue
+++ Starter/app.vue
@@ -1,23 +1,54 @@
 <template>
-  <VitePwaManifest />
   <NuxtLayout>
-    <index />
+    <NuxtPage />
   </NuxtLayout>
 </template>
 
-<script>
-// Import the index.vue component
-import Index from './pages/index.vue'
-
-export default {
-  name: 'App',
-  components: {
-    // Register the index.vue component
-    Index
-  }
+<style lang="postcss">
+@keyframes fadeIn {
+  0% {opacity: 0.001}
+  100% {opacity: 1}
 }
-</script>
-
-<style>
-/* Global styles for your app can go here */
+@keyframes fadeOut {
+  0% { opacity: 1}
+  100% {opacity: 0.001}
+}
+.page-enter-active,
+.page-leave-active,
+.layout-enter-active,
+.layout-leave-active {
+  transition: opacity 250ms;
+}
+.page-enter,
+.page-leave-to,
+.layout-enter,
+.layout-leave-to {
+  opacity: 0;
+}
+.page-enter-active,
+.layout-enter-active {
+  animation-duration: 250ms;
+  animation-name: fadeIn;
+  animation-timing-function: linear;
+  backface-visibility: hidden;
+}
+.page-leave-active,
+.layout-leave-active {
+  animation-name: fadeOut;
+  animation-duration: 0.25s;
+}
+/* Scale Y */
+.scale-y-enter-active,
+.scale-y-leave-active {
+  transition: all 300ms linear;
+  will-change: max-height, opacity;
+  max-height: 160px;
+  overflow: hidden;
+  opacity: 1;
+}
+.scale-y-enter-from,
+.scale-y-leave-to {
+  max-height: 0;
+  opacity: 0;
+}
 </style>
--- Starter/layouts/default.vue
+++ Starter/layouts/default.vue
@@ -1,28 +1,14 @@
 <template>
-    <div class="container mx-auto p-4">
-      <header>
-        <h1 class="text-4xl font-bold mb-4">My Nuxt 3 App</h1>
-      </header>
-      <NavigationTail />
-      <!-- This is where the content of your pages will be rendered -->
-      <slot />
-      
-      <footer>
-        <p class="text-center mt-4">© 2023 My Nuxt App</p>
-      </footer>
-    </div>
-  </template>
-  
-  <script>
-import NavigationTail from '../components/NavigationTail.vue';
-
-  export default {
-    name: 'DefaultLayout',
-    components: { NavigationTail }
-}
-  </script>
-  
-  <style>
-  /* Global styles for your layout can go here */
-  </style>
-  
+  <main>
+    <section class="pt-20 pb-28 min-h-screen flex flex-col justify-center">
+      <div class="px-6 w-full mx-auto max-w-6xl">
+        <slot />
+        <div class="absolute inset-0">
+          <div class="background-gradient">
+            <div class="background-gradient-pattern"></div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
