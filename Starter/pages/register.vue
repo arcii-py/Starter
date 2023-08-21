b94b1b473b0d98159df2aad2227ea8715ea75c74
arcii-py <63791186+arcii-py@users.noreply.github.com>
2023-08-21 19:45:54 +0200

    styled register.vue

--- Starter/pages/register.vue
+++ Starter/pages/register.vue
@@ -1,59 +1,51 @@
 <template>
-    <div class="DaoRb">
-      <h1 class="eSHwvX">Create an account</h1>
+  <div class="min-h-screen flex items-center justify-center bg-gray-100">
+    <div class="bg-white p-8 rounded-lg shadow-md w-96">
+      <h1 class="text-2xl font-semibold mb-4">Create an account</h1>
       <form @submit.prevent="signUp">
         <ErrorAlert :error-msg="authError" @clearError="clearError" />
-        <div class="jGQTZC">
-          <label class="iJLvzO">
-            <div class="fdCSlG">
-              <input class="cmCuLh" type="text" placeholder="First name" v-model="name" />
-            </div>
-          </label>
-          <label class="iJLvzO">
-            <div class="fdCSlG">
-              <input class="cmCuLh" type="text" placeholder="Last name" v-model="lastname" />
-            </div>
-          </label>
-          <label class="iJLvzO">
-            <div class="fdCSlG">
-              <input class="cmCuLh" type="text" placeholder="Company (Optional)" v-model="company" />
-            </div>
-          </label>
-          <label class="iJLvzO">
-            <div class="fdCSlG">
-              <input class="cmCuLh" type="text" placeholder="Email address" v-model="email" />
-            </div>
-          </label>
-          <label class="iJLvzO">
-            <div class="fdCSlG">
-              <input class="cmCuLh" type="password" placeholder="Password" v-model="password" />
-            </div>
-          </label>
-        </div>
-        <div class="jGQTZC">
-          <button class="gZMQdu" type="submit" :disabled="loading">
-            <div class="bjhGPG" :class="{loading: loading}">Sign up</div>
-            <svg viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" class="jjoFVh" :class="{loading: loading}">
-              <g fill="none" stroke-width="1.5" stroke-linecap="round" class="faEWLr" style="stroke: var(--icon-color);">
-                <circle stroke-opacity=".2" cx="8" cy="8" r="6"></circle>
-                <circle cx="8" cy="8" r="6" class="VFMrX"></circle>
-              </g>
-            </svg>
-          </button>
-          <div class="xxEKN">
-            By signing up you agree to our
-            <a href="https://policies.google.com/terms" target="_blank" rel="noopener noreferrer" class="bkFclS">
-              <span>API Terms of Service</span>
-            </a>
-            and
-            <a href="https://policies.google.com/privacy" target="_blank" rel="noopener noreferrer" class="bkFclS">
-              <span>Privacy Policy</span>
-            </a>.
+        <div class="space-y-4">
+          <div>
+            <label class="block text-sm font-medium text-gray-600 mb-2">First name</label>
+            <input class="w-full p-2 border rounded-md" type="text" placeholder="First name" v-model="name" />
           </div>
+          <div>
+            <label class="block text-sm font-medium text-gray-600 mb-2">Last name</label>
+            <input class="w-full p-2 border rounded-md" type="text" placeholder="Last name" v-model="lastname" />
+          </div>
+          <div>
+            <label class="block text-sm font-medium text-gray-600 mb-2">Company (Optional)</label>
+            <input class="w-full p-2 border rounded-md" type="text" placeholder="Company" v-model="company" />
+          </div>
+          <div>
+            <label class="block text-sm font-medium text-gray-600 mb-2">Email address</label>
+            <input class="w-full p-2 border rounded-md" type="text" placeholder="Email address" v-model="email" />
+          </div>
+          <div>
+            <label class="block text-sm font-medium text-gray-600 mb-2">Password</label>
+            <input class="w-full p-2 border rounded-md" type="password" placeholder="Password" v-model="password" />
+          </div>
+        </div>
+        <div class="mt-6">
+          <button class="w-full bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600" type="submit" :disabled="loading">
+            Sign up
+          </button>
+        </div>
+        <div class="mt-4 text-center text-sm">
+          By signing up you agree to our
+          <a href="https://policies.google.com/terms" target="_blank" rel="noopener noreferrer" class="text-blue-500 hover:underline">
+            API Terms of Service
+          </a>
+          and
+          <a href="https://policies.google.com/privacy" target="_blank" rel="noopener noreferrer" class="text-blue-500 hover:underline">
+            Privacy Policy
+          </a>.
         </div>
       </form>
     </div>
-  </template>
+  </div>
+</template>
+
   
   <script setup lang="ts">
   definePageMeta({
